0016fc3464d58fce6b59b2bfabb5126e1e6415b7
cromasdev <58916537+cromasdev@users.noreply.github.com>
2020-03-03 07:27:50 -0300

    WIPE

--- Files/scipts/onload.js
+++
@@ -1,7 +0,0 @@
-  window.onload = function() {
-      if(!window.location.hash) {
-          window.location = window.location + '#ready';
-          window.location.reload();
-      }
-      checkCookie();
-  }
--- pages/Audio.bat
+++
@@ -1,7 +0,0 @@
-  
-
-      
-
-REG DELETE "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoControlPanel /f
-mmsys.cpl
-pause
